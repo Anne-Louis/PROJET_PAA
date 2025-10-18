6cba381db7afa36e9e7b7b66d42b2c1206d24de0
unknown <aali0louz@gmail.com>
2025-10-18 23:47:30 +0200

    Premiere version de la class Maison

--- src/main/Maison.java
+++ src/main/Maison.java
@@ -1,5 +1,40 @@
 package main;
 
 public class Maison {
-    
+    private String nom;
+    private NiveauConsommation niveau;
+    private Generateur generateur;
+
+    public House(String nom, NiveauConsommation niveau) {
+        this.nom = nom;
+        this.niveau = niveau;
+        this.generateur = null;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public NiveauConsommation getNiveau() {
+        return niveau;
+    }
+
+    public Generateur getGenerateur() {
+        return generateur;
+    }
+
+    public void setGenerator(Generateur generateur) {
+        this.generateur = generateur;
+    }
+
+    public int getConsommation() {
+        return niveu.getValeur();
+    }
+
+    @Override
+    public String toString() {
+        String genNom = (generateur != null) ? generateur.getNom() : "Aucun";
+        return "Maison " + nom +
+               " (" + niveau + ", " + getConsommation() + " kW) → Générateur : " + genNom;
+    }
 }
---
+++ src/main/NiveauConsommation.java
@@ -0,0 +1,22 @@
+package main;
+
+/* enumeration pour les niveau de consommation des maison les valeur de consommation affecter a leur categories :) */
+public enum NiveauConsommation {
+    BASSE(10),
+    NORMALE(20),
+    FORTE(40);
+
+    private final int valeur;
+
+    NiveauConsommation(int valeur) {
+        this.valeur = valeur;
+    }
+
+    public int getValeur() {
+        return valeur;
+    }
+
+    /*public int setValeur(int valeur){
+
+    }*/
+}
