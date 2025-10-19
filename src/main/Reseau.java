f5c2e0b91fca71c4311d477efdc62b02537b0d8c
Sogolon88 <166301537+Sogolon88@users.noreply.github.com>
2025-10-20 00:42:03 +0200

    Refactor Reseau class with constructors and methods

--- src/main/Reseau.java
+++ src/main/Reseau.java
@@ -1,5 +1,97 @@
 package main;
+import java.util.List;
+import java.util.ArrayListe;
 
 public class Reseau {
+    private liste<Generateur> generateurs;
+    private liste<Maison> maisons;
+    private Liste<Connexion> connexions;
     
+    public Reseau(){
+        generateurs = new ArrayListe<Generateur>();
+        maisons = new ArrayListe<Maison>();
+        connexions = new ArrayListe<Connexion>();
+    }
+
+    public Reseau(liste<Generateur> gens, liste<Maison> msns, liste<Connexion> conns){
+        this.generateurs = gens;
+        this.maisons = msns;
+        this.connexions = conns;
+    }
+    public Reseau(Reseau r){
+        this.generateurs = r.getGenerateurs();
+        this.maisons = r.getMaisons();
+        this.connexions = r.getConnexions();
+    }
+
+    public double calculerCoutReseau(){
+    //implementation à faire
+        return 0.0;
+    }
+
+    public boolean modifierConnexion(Connexion oldConn, Connexion newConn){
+        int index = this.connexions.indexOf(oldConn);
+        if (index == -1 || newConn == null)
+            return false;
+        this.connexions.set(index, newConn);
+        return true;
+    }
+
+    public boolean validerReseau(){
+        double totalCapacite = 0;
+        double totalConsommation = 0;
+        int nbConnexions = 0;
+
+        if (this.generateurs.size() == 0 || this.maisons.size() == 0)
+            return false;
+        for (Generateur gen : this.generateurs)
+            totalCapacite += gen.getCapacite();
+        for (Maison msn : this.maisons){
+            totalConsommation += msn.getConsommation();
+            for(Connexion cons : this.connexions)
+                if (cons.getMaison().equals(msn))
+                    nbConnexions++;
+            if (nbConnexions != 0) 
+                return false;      
+        }
+        
+        return (totalCapacite >= totalConsommation);
+        return true;
+    } 
+    public boolean ajouterMaison(Maison msn){
+        if (msn == null)
+            return false;
+        return this.maisons.add(msn);
+    }
+
+    public boolean ajouterGenerateur(Generateur gen){
+        if (gen == null)
+            return false;
+        return this.generateurs.add(gen);
+    }
+    public boolean ajouterConnexion(Connexion conn){
+        if (conn == null)
+            return false;
+        return this.connexions.add(conn);
+    }
+
+
+    public getGenerateurs(){
+        return this.generateurs;
+    }
+    public getMaisons(){
+        return this.maisons;
+    }
+    public getConnexions(){
+        return this.connexions;
+    }
+    public void setMaisons(liste<Maison> msns){
+        this.maisons = msns;
+    }
+    public void setConnexions(liste<Connexion> conns){
+        this.connexions = conns;
+    }
+    public void setGenerateurs(liste<Generateur> gens){
+        this.generateurs = gens;
+    }
 }
