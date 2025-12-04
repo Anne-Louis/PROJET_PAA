d0d258419ae1b8549087e4e518243b32a624f793
Djata <166301537+Sogolon88@users.noreply.github.com>
2025-12-04 12:38:53 +0100

    Amélioration du Menu3 pour tester les algorithmes

--- src/main/menus/Menu3.java
+++ src/main/menus/Menu3.java
@@ -3,6 +3,7 @@ package main.menus;
 import java.util.Scanner;
 import main.components.Reseau;
 import main.algorithmes.*;
+import main.io.ParserFile;
 
 /**
  * Menu tertiaire d'optimisation du réseau (menu 3).
@@ -24,25 +25,43 @@ public class Menu3 {
         while (!fin){
             System.out.println("-------------------- Gestion réseau --------------------");
             System.out.println("----------- Optimisation d'un réseau (Menu 3) ---------");
-            System.out.println("1) résolution automatique") ;
-            System.out.println("2) sauvegarder la solution actuelle;") ;
-            System.out.println("3) fin.") ;
+            System.out.println("0 Charger un fichier de test");
+            System.out.println("1) résolution automatique 1") ;
+            System.out.println("2) resolution automatique 2") ;
+            System.out.println("3) sauvegarder la solution actuelle;") ;
+            System.out.println("4) Calculer cout reseau.") ;
+            System.out.println("5) affichiser le reseau.");
+            System.out.println("6) fin");
             System.out.println("--------------------------------------------------------");
 
             int nb = UtilMenu.lireEntierAuClavier(sc, "Votre choix ? : ");
             sc.nextLine();
 
             switch(nb){
+                case 0:
+                    reseau = ParserFile.readReseau();
+                    break;
                 case 1 :
-                    Algorithme1.resoudreReseau(reseau, Algorithme1.epsilonInit );
+                    Algorithme1.resoudreReseau(reseau, Algorithme1.epsilonInit);
+                    System.out.println("Le cout du reseau vaut : "+ reseau.calculerCoutReseau());
                     break ;
                 case 2 :
-                    UtilMenu.sauvegardeSolution(reseau, sc);
+                    reseau = Algorithme2.resoudreReseau(reseau, Algorithme1.epsilonInit);
+                    System.out.println("Le cout du reseau vaut : "+ reseau.calculerCoutReseau());
                     break ;
                 case 3 :
-                    fin = true ;
-                    System.out.println("Merci d'avoir utilisé ce programme !!!") ;
+                    UtilMenu.sauvegardeSolution(reseau, sc);
                     break ;
+                case 4 :
+                    System.out.println("Le cout du reseau vaut : "+ reseau.calculerCoutReseau());
+                    break ;
+                case 5 :
+                    System.out.println(reseau.toString());
+                    break;
+                case 6 :
+                    fin = true;
+                    System.out.println("Merci d'avoir utilisé ce programme !!!") ;
+                    break;
                 default :
                     System.out.println("Ce n'est pas un choix valide !");
             }
