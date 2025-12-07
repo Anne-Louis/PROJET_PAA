25c5966846d59c110472383cc1017ef1fc089fdf
Djata <166301537+Sogolon88@users.noreply.github.com>
2025-12-07 21:33:58 +0100

    ajout de la class exception pour la validation du reseau

--- src/main/exceptions/InvalideReseauException.java
+++ src/main/exceptions/InvalideReseauException.java
@@ -1,1 +1,13 @@
+package main.exceptions;
 
+public class InvalideReseauException extends Exception {
+    
+    public InvalideReseauException(String message){
+        super(message);
+    }
+
+    public InvalideReseauException(String message, Throwable cause){
+        super(message, cause);
+    }
+    
+}
