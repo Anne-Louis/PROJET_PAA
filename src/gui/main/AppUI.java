d04dbf66d638c0df10bcd3400711d42a8be34358
Djata <166301537+Sogolon88@users.noreply.github.com>
2025-12-17 23:50:36 +0100

    point d'entrer de l'application javafx
    This class serves as the entry point for the JavaFX GUI application, initializing and displaying the main window.

---
+++ src/gui/main/AppUI.java
@@ -0,0 +1,34 @@
+package gui.main;
+
+import gui.views.MainWindows;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Point d’entrée de l’interface graphique JavaFX.
+ */
+public class AppUI extends Application {
+
+    /**
+     * Initialise et affiche la fenêtre principale.
+     */
+    @Override
+    public void start(Stage primaryScene) throws Exception {
+        MainWindows mainWdws = new MainWindows();
+
+        Scene scene = new Scene(mainWdws);
+
+        primaryScene.setTitle("Reseau");
+        primaryScene.setScene(scene);
+        primaryScene.sizeToScene();
+        primaryScene.show();
+    }
+
+    /**
+     * Lance l’application JavaFX.
+     */
+    public static void main(String[] args){
+        launch(args);
+    }
+}
